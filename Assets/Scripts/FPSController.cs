1f89e5b4ca91f7a4177492351576b72bb2af0f30
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2024-10-28 14:58:11 -0500

    Setup FPS basic controls
    Move, sprint, look and jump mapped to Input action system
    Works with keyboard/mouse and gamepad

--- Assets/Scripts/FPSController.cs
+++ Assets/Scripts/FPSController.cs
@@ -1,74 +1,124 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 [RequireComponent(typeof(CharacterController))]
-
-public class SC_FPSController : MonoBehaviour
+public class FPSController : MonoBehaviour
 {
-     public float walkingSpeed = 7.5f;
-     public float runningSpeed = 11.5f;
-     public float jumpSpeed = 8.0f;
-     public float gravity = 20.0f;
-     public Camera playerCamera;
-     public float lookSpeed = 2.0f;
-     public float lookXLimit = 45.0f;
+     #region Input actions
+     public InputSystem_Actions playerControls;
+     private InputAction moveAction;
+     private InputAction jumpAction;
+     private InputAction rotateAction;
+     private InputAction sprintAction;
+     #endregion
 
-     CharacterController characterController;
-     Vector3 moveDirection = Vector3.zero;
-     float rotationX = 0;
+     public Camera playerCamera;
+     public float walkSpeed = 6f;
+     public float runSpeed = 12f;
+     public float jumpPower = 8.0f;
+     public float gravity = 20.0f;
+
+     public float lookSpeed = 1f;
+     public float lookXLimit = 45f;
+     private const float LOOK_SPEED_SCALAR = 0.1f;
+
+     private CharacterController characterController;
+
+     private Vector3 moveDirection = Vector3.zero;
+     private Vector3 inputDirection = Vector3.zero;
+     private Vector3 inputRotation = Vector3.zero;
+     private float rotationX = 0;
 
      [HideInInspector]
      public bool canMove = true;
 
-     void Start()
+     private void Awake()
      {
-          characterController = GetComponent<CharacterController>();
+          playerControls = new InputSystem_Actions();
+     }
 
-          // Lock cursor
+     private void OnEnable()
+     {
+          moveAction = playerControls.Player.Move;
+          moveAction.Enable();
+
+          sprintAction = playerControls.Player.Sprint;
+          sprintAction.Enable();
+
+          jumpAction = playerControls.Player.Jump;
+          jumpAction.Enable();
+          jumpAction.performed += Jump;
+
+          rotateAction = playerControls.Player.Look;
+          rotateAction.Enable();
+     }
+
+     private void OnDisable()
+     {
+          moveAction.Disable();
+          jumpAction.Disable();
+          rotateAction.Disable();
+          sprintAction.Disable();
+     }
+
+     void Start()
+     { 
+          characterController = GetComponent<CharacterController>();
           Cursor.lockState = CursorLockMode.Locked;
           Cursor.visible = false;
      }
 
      void Update()
      {
-          // We are grounded, so recalculate move direction based on axes
+          HandleMovement();
+
+          HandleRotation();
+     }
+
+     private void HandleMovement()
+     {
+          inputDirection = moveAction.ReadValue<Vector2>();
           Vector3 forward = transform.TransformDirection(Vector3.forward);
           Vector3 right = transform.TransformDirection(Vector3.right);
-          // Press Left Shift to run
-          bool isRunning = Input.GetKey(KeyCode.LeftShift);
-          float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
-          float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+
+          bool isRunning = sprintAction.IsPressed();
+          float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * inputDirection.y : 0;
+          float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * inputDirection.x : 0;
           float movementDirectionY = moveDirection.y;
           moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+          moveDirection.y = movementDirectionY;
 
-          if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
-          {
-               moveDirection.y = jumpSpeed;
-          }
-          else
-          {
-               moveDirection.y = movementDirectionY;
-          }
+          GravityTick();
 
-          // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
-          // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
-          // as an acceleration (ms^-2)
+          characterController.Move(moveDirection * Time.deltaTime);
+     }
+
+     private void HandleRotation()
+     {
+          if (canMove)
+          {
+               inputRotation = rotateAction.ReadValue<Vector2>();
+               rotationX += -inputRotation.y * lookSpeed;
+               rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+               playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+               transform.rotation *= Quaternion.Euler(0, inputRotation.x * lookSpeed, 0);
+          }
+     }
+
+     private void Jump(InputAction.CallbackContext context)
+     {
+          if (canMove && characterController.isGrounded)
+          {
+               moveDirection.y = jumpPower;
+          }
+     }
+
+     private void GravityTick()
+     {
           if (!characterController.isGrounded)
           {
                moveDirection.y -= gravity * Time.deltaTime;
-          }
-
-          // Move the controller
-          characterController.Move(moveDirection * Time.deltaTime);
-
-          // Player and Camera rotation
-          if (canMove)
-          {
-               rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
-               rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
-               playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
-               transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
           }
      }
 }
