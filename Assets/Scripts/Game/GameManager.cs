86aeecd51f4d91becf78c9b902bb9ceda6c054bb
JacobTucker22 <72776180+JacobTucker22@users.noreply.github.com>
2024-11-08 17:19:34 -0600

    Added level 2 completion

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,13 +1,16 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class GameManager : MonoBehaviour
 {
      public static GameManager Instance;
 
      public List<LevelManager> Levels = new List<LevelManager>();
+     [SerializeField] private List<LevelManager> completedLevels = new List<LevelManager>();
 
      [SerializeField] private LevelManager currentLevel;
+     private int currentLevelIndex = 0;
 
      private void Awake()
      {
@@ -32,6 +35,21 @@ public class GameManager : MonoBehaviour
 
      private void OnLevelComplete(LevelManager level)
      {
-          Debug.Log("Level Complete!");
+          if(Levels.Contains(level))
+          {
+               completedLevels.Add(level);
+               Levels.Remove(level);
+          }
+          if(Levels.Count == 0)
+          {
+               Debug.Log("Game Finished!!!");
+               // End game Sequence
+               return;
+          }
+          // Level start sequence/ transition
+          currentLevel = Levels[0];
+          Debug.Log("Starting level: " + currentLevel.LevelTitle + "\nGood Luck...");
+
+          currentLevel.StartLevelSequence();
      }
 }
--- Assets/Scripts/Game/LevelManager.cs
+++ Assets/Scripts/Game/LevelManager.cs
@@ -1,17 +1,21 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System.Collections;
 using System.Collections.Generic;
 
 public class LevelManager : MonoBehaviour
-{ 
+{
+     public string LevelTitle = "New Level";
      public List<GameObject> Enemies = new List<GameObject>();
      public int EnemyCount => Enemies.Count;
      public bool isEnemiesCleared => EnemyCount == 0;
      public UnityAction<LevelManager> LevelCompleteEvent;
 
      public InteractableDoor LevelDoor;
+     public InteractableDoor LevelStartDoor;
 
      public CompletionZone LevelCompletionZone;
+     public Transform StartPoint;
 
      private void Awake()
      {
@@ -44,7 +48,26 @@ public class LevelManager : MonoBehaviour
 
      private void OnCompletionZone()
      {
-          Debug.Log("Level Complete!");
+          Debug.Log("Level " + LevelTitle + " Complete!!!");
           LevelCompleteEvent?.Invoke(this);
+          // Wrap up level as game manager transitions
+     }
+
+     // TODO: Probably better way than teleporting to start next level
+     // Maybe an animation and a timer for door unlocks.
+     public void StartLevelSequence()
+     {
+          LevelStartDoor.isLocked = true;
+          GameObject player = GameObject.Find("Player");
+          player.GetComponent<CharacterController>().enabled = false;
+          player.transform.position = StartPoint.position;
+          player.GetComponent<CharacterController>().enabled = true;
+          StartCoroutine(SlowUnlockDoor());
+     }
+
+     private IEnumerator SlowUnlockDoor()
+     {
+          yield return new WaitForSeconds(5.0f);
+          LevelStartDoor.isLocked = false;
      }
 }
