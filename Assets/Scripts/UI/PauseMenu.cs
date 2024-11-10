dde4cb3d93b58237201629f868a77a70656e8b20
tynanbarlow <tynanbarlow@gmail.com>
2024-11-09 16:30:41 -0800

    Pause Menu

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -1,12 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
-{
-    
-    public void PlayGame ()
+public class PauseMenu : MonoBehaviour
+{   
+    public void ResumeGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
