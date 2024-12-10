7a25039005d5b8903eb7e783eb5bebd5c1be3126
tynanbarlow <tynanbarlow@gmail.com>
2024-12-10 16:20:53 -0700

    updates to health and stamina images

---
+++ Assets/Scripts/HUD/EnemyHealthControl.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class EnemyHealthControl : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public List<Texture> healthStatus;
+
+    private LevelManager levelManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //set the amount of cookies active
+        //levelManager = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //when called, sets the correct cookie to the right image
+    public void HealthStatus()
+    {
+        return;
+    }
+}
