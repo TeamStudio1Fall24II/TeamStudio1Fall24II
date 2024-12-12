84f0a704eb25d2f3f6adbdfbe7e8570897e0749d
Carly <cmeyerhardt@gmail.com>
2024-12-11 16:16:30 -0800

    Reorganized Level Hierarchy
    You're welcome :)

--- Assets/Scripts/Game/PlayerTriggerZone.cs
+++ Assets/Scripts/Game/PlayerTriggerZone.cs
@@ -8,12 +8,12 @@ public class PlayerTriggerZone : MonoBehaviour
      public UnityAction PlayerExitedTriggerZone;
 
     private void OnTriggerEnter(Collider other)
-     {
+    {
           if(other.CompareTag("Player"))
           {
                PlayerEnteredTriggerZone?.Invoke();
           }
-     }
+    }
 
     private void OnTriggerExit(Collider other)
     {
